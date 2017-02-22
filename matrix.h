94c9a8a9e198a4dbbdd18407265284b5c02791f5
CastleRun <786901a@gmail.com>
2017-02-21 16:25:15 -0800

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -3,3 +3,18 @@ Matrix.h
 
 Matrix.h is a matrix library for C.
 */
+
+#include <stdio.h>
+
+/*
+*mat is a pointer to the matrix, with r and c being the matrix's dimensions
+
+When matrix is created, (r*c) memory values are allocated.
+*/
+typedef struct{
+  int *mat;
+  int c;
+  int r;
+} matrix;
+
+//createMatrix goes here
